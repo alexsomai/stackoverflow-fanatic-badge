7cd69a89a486f873810e011d53fe4b172d95e551
Alexandru Somai <somai.alexandru@gmail.com>
2018-01-25 15:51:46 +0200

    Initial commit dummy clock.py file

---
+++ clock.py
@@ -0,0 +1,13 @@
+from apscheduler.schedulers.blocking import BlockingScheduler
+
+sched = BlockingScheduler()
+
+@sched.scheduled_job('interval', minutes=3)
+def timed_job():
+    print('This job is run every three minutes.')
+
+@sched.scheduled_job('cron', day_of_week='mon-fri', hour=17)
+def scheduled_job():
+    print('This job is run every weekday at 5pm.')
+
+sched.start()
