a47775107465bc7d549246539645b0229bdfda52
Alexandru Somai <somai.alexandru@gmail.com>
2018-01-26 16:28:22 +0200

    Make api call to user profile on stackoverflow

--- clock.py
+++ clock.py
@@ -1,11 +1,22 @@
-from apscheduler.schedulers.blocking import BlockingScheduler
+import requests
 import os
+
+from apscheduler.schedulers.blocking import BlockingScheduler
+from pprint import pprint
 
 sched = BlockingScheduler()
 
-@sched.scheduled_job('interval', minutes=1)
-def timed_job():
-    print(os.environ['STACK_EXCHANGE_ACCESS_TOKEN'])
-    print(os.environ['STACK_EXCHANGE_KEY'])
+@sched.scheduled_job('interval', minutes=15)
+def access_stackoverflow():
+    site = 'stackoverflow.com'
+    key = os.environ['STACK_EXCHANGE_KEY']
+    access_token = os.environ['STACK_EXCHANGE_ACCESS_TOKEN']
+
+    profile_page_api = 'https://api.stackexchange.com/2.2/me'
+    url = profile_page_api + '?' + 'site=' + site + '&key=' + key + '&access_token=' + access_token
+
+    response = requests.get(url)
+    json = response.json()
+    pprint(json)
 
 sched.start()
