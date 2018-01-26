f114dca130c11154e11e4dab90b4600881918886
Alexandru Somai <somai.alexandru@gmail.com>
2018-01-26 15:35:09 +0200

    Syntax error fix

--- clock.py
+++ clock.py
@@ -5,7 +5,7 @@ sched = BlockingScheduler()
 
 @sched.scheduled_job('interval', minutes=1)
 def timed_job():
-    print(os.environ['STACK_EXCHANGE_ACCESS_TOKEN']
+    print(os.environ['STACK_EXCHANGE_ACCESS_TOKEN'])
     print(os.environ['STACK_EXCHANGE_KEY'])
 
 sched.start()
