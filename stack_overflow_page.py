d28a5f364d9517a7bb00e16b494edb58b0482fca
Alexandru Somai <somai.alexandru@gmail.com>
2018-01-28 19:15:55 +0200

    set --headless

--- stack_overflow_page.py
+++ stack_overflow_page.py
@@ -9,9 +9,10 @@ def login():
     chrome_options = Options()
     chrome_location = os.environ['GOOGLE_CHROME_BIN']
     print(chrome_location)
-    ch = os.environ['GOOGLE_CHROME_SHIM']
-    print(ch)
+    # ch = os.environ['GOOGLE_CHROME_SHIM']
+    # print(ch)
     chrome_options.binary_location = chrome_location
+    chrome_options.add_argument('--headless')
     chrome_options.add_argument('--disable-gpu')
     chrome_options.add_argument('--no-sandbox')
     driver = webdriver.Chrome(executable_path=os.environ['CHROMEDRIVER_PATH'], chrome_options=chrome_options)
