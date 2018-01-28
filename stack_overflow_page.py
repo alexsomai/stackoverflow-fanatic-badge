867b1dffa4ed1d07e984aeb90ad33223650cdfbe
Alexandru Somai <somai.alexandru@gmail.com>
2018-01-28 19:51:32 +0200

    Run selenium on heroku

--- stack_overflow_page.py
+++ stack_overflow_page.py
@@ -6,23 +6,12 @@ from selenium.webdriver.chrome.options import Options
 
 def login():
     print("Logging into stackoverflow.com")
-    print("Printing ... ")
-    chrome_location = os.environ.get('GOOGLE_CHROME_BIN')
-    print(chrome_location)
-    chrome_shim = os.environ.get('GOOGLE_CHROME_SHIM')
-    print(chrome_shim)
+    chrome_location = os.environ.get('GOOGLE_CHROME_SHIM')
     chrome_options = Options()
-    chrome_options.binary_location = chrome_shim
-    # chrome_options.add_argument('--headless')
-    # chrome_options.add_argument('--disable-gpu')
-    # chrome_options.add_argument('--no-sandbox')
-    # chrome_options.add_argument('--remote-debugging-port=9222')
+    chrome_options.binary_location = chrome_location
     driver = webdriver.Chrome(chrome_options=chrome_options)
-    # driver = webdriver.Chrome()
     try:
-        print("Initialized driver")
         driver.get("https://stackoverflow.com")
-        print("Got on SO page")
 
         driver.find_element_by_link_text("Log In").click()
 
