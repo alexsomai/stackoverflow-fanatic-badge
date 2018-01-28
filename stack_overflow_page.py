331b2e80029a3b4ccd8a7055561e858e002d571e
Alexandru Somai <somai.alexandru@gmail.com>
2018-01-28 19:46:47 +0200

    More testing

--- stack_overflow_page.py
+++ stack_overflow_page.py
@@ -13,10 +13,10 @@ def login():
     print(chrome_shim)
     chrome_options = Options()
     chrome_options.binary_location = chrome_shim
-    chrome_options.add_argument('--headless')
-    chrome_options.add_argument('--disable-gpu')
-    chrome_options.add_argument('--no-sandbox')
-    chrome_options.add_argument('--remote-debugging-port=9222')
+    # chrome_options.add_argument('--headless')
+    # chrome_options.add_argument('--disable-gpu')
+    # chrome_options.add_argument('--no-sandbox')
+    # chrome_options.add_argument('--remote-debugging-port=9222')
     driver = webdriver.Chrome(chrome_options=chrome_options)
     # driver = webdriver.Chrome()
     try:
