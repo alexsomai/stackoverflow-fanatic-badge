5a95dec6612b7930aac4217e0e0ce16b531bc4ae
Alexandru Somai <somai.alexandru@gmail.com>
2018-01-28 19:18:21 +0200

    More changes

--- stack_overflow_page.py
+++ stack_overflow_page.py
@@ -9,12 +9,11 @@ def login():
     chrome_options = Options()
     chrome_location = os.environ['GOOGLE_CHROME_BIN']
     print(chrome_location)
-    # ch = os.environ['GOOGLE_CHROME_SHIM']
-    # print(ch)
     chrome_options.binary_location = chrome_location
     chrome_options.add_argument('--headless')
     chrome_options.add_argument('--disable-gpu')
     chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--remote-debugging-port=9222')
     driver = webdriver.Chrome(executable_path=os.environ['CHROMEDRIVER_PATH'], chrome_options=chrome_options)
     # driver = webdriver.Chrome()
     try:
