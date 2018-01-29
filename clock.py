3a89b64d2eb4614260de9f141b5864ed289edd2f
Alexandru Somai <somai.alexandru@gmail.com>
2018-01-29 17:26:03 +0200

    Clean scheduler impl

--- clock.py
+++ clock.py
@@ -1,7 +1,3 @@
-import os
-from pprint import pprint
-
-import requests
 from apscheduler.schedulers.blocking import BlockingScheduler
 
 import stack_overflow_page
@@ -12,21 +8,6 @@ schedule = BlockingScheduler()
 @schedule.scheduled_job('interval', hours=8)
 def access_stack_overflow_page():
     stack_overflow_page.login()
-
-
-# Temporary remove - the API will be used to verify last_access_date < 24h
-# @sched.scheduled_job('interval', minutes=15)
-# def access_stackoverflow():
-#     site = 'stackoverflow.com'
-#     key = os.environ['STACK_EXCHANGE_KEY']
-#     access_token = os.environ['STACK_EXCHANGE_ACCESS_TOKEN']
-#
-#     profile_page_api = 'https://api.stackexchange.com/2.2/me'
-#     url = profile_page_api + '?' + 'site=' + site + '&key=' + key + '&access_token=' + access_token
-#
-#     response = requests.get(url)
-#     json = response.json()
-#     pprint(json)
 
 
 schedule.start()
