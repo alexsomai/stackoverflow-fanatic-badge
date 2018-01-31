362be0d2d7938ea34a0cfcc3dc15dcceb901b6d6
Alexandru Somai <somai.alexandru@gmail.com>
2018-01-31 09:53:10 +0200

    Change scheduling

--- clock.py
+++ clock.py
@@ -14,7 +14,7 @@ def access_stack_overflow_page():
     stack_overflow_page.login()
 
 
-@schedule.scheduled_job('interval', hours=6)
+@schedule.scheduled_job('interval', hours=1)
 def access_stack_overflow_api():
     user_details = stack_exchange_api.get_user_details()
     last_access_date_timestamp = user_details['items'][0]['last_access_date']
