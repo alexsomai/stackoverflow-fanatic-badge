20c00718e3fa66f001aa0ecd05960e2d1eca215a
Alexandru Somai <somai.alexandru@gmail.com>
2018-01-31 09:32:11 +0200

    Change email alerts and schedules

--- sendgrid_helper.py
+++ sendgrid_helper.py
@@ -3,12 +3,11 @@ import os
 from sendgrid.helpers.mail import *
 
 
-def send_mail(content):
+def send_mail(subject, content):
     sg = sendgrid.SendGridAPIClient(apikey=os.environ.get('SENDGRID_API_KEY'))
     from_email = Email("heroku-stackoverflow-fanatic-badge@heroku.com")
     to_email = Email(os.environ.get('STACK_OVERFLOW_EMAIL'))
     content = Content("text/plain", content)
-    subject = "Mail from Heroku stackoverflow-fanatic-badge app"
     body = Mail(from_email, subject, to_email, content)
     response = sg.client.mail.send.post(request_body=body.get())
     print(response.status_code)
