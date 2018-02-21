646f56cf40311463b19fc194e62f48235f1dda22
Alexandru Somai <somai.alexandru@gmail.com>
2018-02-21 09:27:37 +0200

    Change scheduled jobs to run every 3 hours

--- clock.py
+++ clock.py
@@ -7,12 +7,12 @@ import stack_overflow_page
 schedule = BlockingScheduler()
 
 
-@schedule.scheduled_job('interval', hours=1)
+@schedule.scheduled_job('interval', hours=3)
 def access_stack_overflow_page():
     stack_overflow_page.login()
 
 
-@schedule.scheduled_job('interval', hours=1)
+@schedule.scheduled_job('interval', hours=3)
 def access_stack_overflow_api():
     delta_hours = 12
     if stack_exchange_api.have_logged_in(12) is False:
