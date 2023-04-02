1d43201b4e088f96a8f4d15f4839a502adbf31cd
AlirezaPourchali <arp.joker82@gmail.com>
2023-04-02 05:39:37 +0330

    changed loc

--- final/bot.py
+++
@@ -1,7 +0,0 @@
-import subprocess
-import time
-proc = subprocess.Popen(["python", "stack_overflow_page.py"], stdout=subprocess.PIPE)
-out = proc.communicate()[0]
-time.sleep(5)
-print ("asghar")
-print(out.upper())
